42956180f315c887cef570005da4a88397c994fd
Dan Beckett <dan.beckett@door4.com>
2025-06-26 10:59:10 +0100

    fix: add blade to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,46 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	name = "treesitter",
-	build = ":TSUpdate",
-	event = { "BufReadPre", "BufNewFile" },
+    "nvim-treesitter/nvim-treesitter",
+    name = "treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        --"nvim-treesitter/nvim-treesitter-textobjects", -- doesn't work with PHP
+    },
     config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = { enable = true },
-			sync_install = { enable = false },
-			ensure_installed = {
-				"lua",
-				"javascript",
-				"typescript",
-				"php",
-				"phpdoc",
-				"css",
-				"html",
-				"twig",
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            auto_install = { enable = true },
+            sync_install = { enable = false },
+            ensure_installed = {
+                "lua",
+                "javascript",
+                "typescript",
+                "json",
+                "php",
+                "phpdoc",
+                "css",
+                "scss",
+                "html",
+                "blade",
+                "twig",
                 "vim",
-				"vimdoc",
+                "vimdoc",
                 "dockerfile",
                 "gitignore",
                 "markdown",
                 "markdown_inline"
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+            },
+            highlight = { enable = true },
+            indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = "<C-s>",
+                    node_decremental = "<C-backspace>",
+                },
+            },
+        })
+    end,
 }
