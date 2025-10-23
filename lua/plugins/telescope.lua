9a9772619ebdab94054badb4145a196efbbd67b9
Dan Beckett <dan.beckett@door4.com>
2025-10-24 00:29:24 +0100

    feat: add commands to fuzzy search specific filetypes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,6 +20,43 @@ return {
                     end
                 })
             end) -- fuzzy grep no ignore
+            vim.keymap.set("n", "<leader>fe", function()
+                local ext = vim.fn.input("Extension > ")
+                if ext == "" then
+                    return
+                end
+                if ext:sub(1,1) == "." then
+                    ext = ext:sub(2)
+                end
+                local glob_arg = "--glob=*." .. ext
+                builtin.live_grep({
+                    prompt_title = "Grepping through ." .. ext .. " files in project",
+                    additional_args = function()
+                        return {
+                            glob_arg,
+                        }
+                    end
+                })
+            end) -- fuzzy grep project with extension
+            vim.keymap.set("n", "<leader>fE", function()
+                local ext = vim.fn.input("Extension > ")
+                if ext == "" then
+                    return
+                end
+                if ext:sub(1,1) == "." then
+                    ext = ext:sub(2)
+                end
+                local glob_arg = "--glob=*." .. ext
+                builtin.live_grep({
+                    prompt_title = "Grepping through all ." .. ext .. " files in directory",
+                    additional_args = function()
+                        return {
+                            "--no-ignore",
+                            glob_arg,
+                        }
+                    end
+                })
+            end) -- fuzzy grep no ignore with extension
             vim.keymap.set("n", "<C-p>", builtin.git_files, {}) -- fuzzy files only git repo
             vim.keymap.set("n", "<leader>ps", function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ") })
