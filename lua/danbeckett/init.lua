bd9539dbe514293f5b81b3df8cd8bce0125a8963
Dan Beckett <dan.beckett@door4.com>
2025-11-17 22:53:56 +0000

    add shortcut for clearing quickfix list

--- lua/danbeckett/init.lua
+++ lua/danbeckett/init.lua
@@ -3,3 +3,7 @@ print("you'll think of something")
 require("danbeckett.remaps")
 require("danbeckett.sets")
 require("danbeckett.lspconfig")
+
+vim.api.nvim_create_user_command('Cfempty', function()
+  vim.fn.setqflist({}, 'r')
+end, {})
